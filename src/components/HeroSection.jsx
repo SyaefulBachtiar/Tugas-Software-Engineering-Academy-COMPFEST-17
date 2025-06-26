0456bf5eb3faa1da27a6c673aca2a4c0723ab9cf
SyaefulBachtiar <syaefulbachtiar2@gmail.com>
2025-06-26 20:22:13 +0700

    Level 5: User & Admin Dashboard 2) Admin Dashboard 7

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection(){
     return(
         // {/* Bagian Sambutan */}
                 <section className="w-full h-screen flex flex-row flex-wrap sm:flex-nowrap my-[80px] sm:my-0">
-                <div className="w-full sm:w-[80%]  sm:h-full flex flex-col sm:items-center  mx-10 ">
+                <div className="w-full sm:w-[80%]  sm:h-full flex flex-col sm:items-center  mx-10 -my-[200px]">
                   <h2 className="text-3xl sm:text-6xl font-semibold font-inter mb-4">Welcome to SEA Catering</h2>
                   <p className="text-sm sm:text-lg font-inter">
                     We are a catering service that provides healthy, customizable food, with fast delivery to all major cities in Indonesia. SEA Catering is here to support your healthy lifestyle, anytime and anywhere.
